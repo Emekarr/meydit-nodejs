9fd3a27582fdf5916f7c960df3c998cc5984c42b
emekarr <aceanyanwu02@gmail.com>
2023-04-15 22:55:06 +0100

    modified the ExceptionHandler to use the custom response class for processing errors

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -1,23 +1,27 @@
-/*
-|--------------------------------------------------------------------------
-| Http Exception Handler
-|--------------------------------------------------------------------------
-|
-| AdonisJs will forward all exceptions occurred during an HTTP request to
-| the following class. You can learn more about exception handling by
-| reading docs.
-|
-| The exception handler extends a base `HttpExceptionHandler` which is not
-| mandatory, however it can do lot of heavy lifting to handle the errors
-| properly.
-|
-*/
-
 import Logger from '@ioc:Adonis/Core/Logger'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import ServerResponse from '../Utils/Response'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
   constructor() {
     super(Logger)
+  }
+
+  handle(error: any, ctx: HttpContextContract): Promise<any> {
+    if (error?.messages?.errors) {
+      new ServerResponse()
+        .setStatusCode(400)
+        .setMessage('an error occured')
+        .setErrors(...error?.messages?.errors)
+        .respond(ctx)
+      return Promise.resolve()
+    }
+    new ServerResponse()
+      .setStatusCode(400)
+      .setMessage('an error occured')
+      .setErrors(error)
+      .respond(ctx)
+    return Promise.resolve()
   }
 }
