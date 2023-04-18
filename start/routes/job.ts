4adc2995db918569cfd9775db38075b877dcb26f
emekarr <aceanyanwu02@gmail.com>
2023-04-18 01:12:16 +0100

    allow both makers and users to fetch jobs

--- start/routes/job.ts
+++ start/routes/job.ts
@@ -2,4 +2,4 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.post('/api/v1/job', 'JobsController.createJob').middleware('auth')
 
-Route.get('/api/v1/job', 'JobsController.fetchJobs').middleware('auth')
+Route.get('/api/v1/job', 'JobsController.fetchJobs').middleware('auth:maker,user')
